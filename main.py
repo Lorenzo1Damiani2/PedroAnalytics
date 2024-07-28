88d35156fb1dc4c921422bc939f9127fb9101740
Lorenzo Damiani <72091414+Lorenzo1Damiani2@users.noreply.github.com>
2024-07-28 15:00:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def hello_world(request):
     # Database connection details
     db_user = 'root'
     db_password = 'lorenz12/'
-    db_name = 'sudsliceofitaly'
+    db_name = '****'
     db_host = '34.175.168.246'  # Replace with your Cloud SQL instance public IP
 
     create_table_invoices(db_user, db_password, db_host, db_name)
@@ -29,7 +29,7 @@ def hello_world(request):
     # The client must configure the authentication and authorization parameters
     # in accordance with the API server security policy.
     # Check out the Authentication section to retrieve the Access Token
-    configuration.access_token = "a/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyZWYiOiJ4SXkxRzBpdjZKSFNaU0prN2M5eEJHbjBjRDBwakU2WSJ9.BsiVxzGJJs67gm2qQTVIphr4AQSeuo1ErPaViEDRoIs"
+    configuration.access_token = "****"
 
     # for this example we define the token as string, but you should have obtained it in the previous steps
     token = "*****"
